7e6c6f9690d9ec70837d6002c82d0f59171eb187
shivani chaudhary <shivanichaudhary664157@gmail.com>
2025-11-27 23:13:19 +0530

    conflict resolve

---
+++ config/db.js
@@ -0,0 +1,18 @@
+// config/db.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/medibridge", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
